b44caa7c402b8b4d6a42ecada7347823136f545a
Tyler Tomani <ttomani@gmail.com>
2025-02-07 03:39:03 -0500

    main

--- projects/extensions/chatGpt-question/js/toggleSideBar.js
+++ projects/extensions/chatGpt-question/js/toggleSideBar.js
@@ -1,9 +1,11 @@
 import { toggleDraggable } from "./letterFocus-myChatGpt.js";
 const draggable = document.querySelector('.draggable')
+const toggleSideBtn = document.querySelector('#toggleSideBtn')
 toggleSideBtn.onclick = toggleSideBar
 
 function toggleSideBar(){
     draggable.classList.toggle('active')
+    toggleSideBtn.classList.toggle(('active'))
 }
 
 draggable.addEventListener('click', e => {
