887024d17f95b2ae6046f0ff0a3c1ed52b93d16a
Tyler Tomani <ttomani@gmail.com>
2025-04-01 13:08:16 -0400

    main

--- webPages/chatGpt-question/js/letterFocus-myChatGpt.js
+++ webPages/chatGpt-question/js/letterFocus-myChatGpt.js
@@ -4,13 +4,13 @@ export function letterFocus() {
     let currentLetter = '';
     const questionAnswers = document.querySelectorAll('.question-answer')
     let questionAnswersFocused = false
+    const allAnswerH3s = document.querySelectorAll('.answer-txt h3')
 
-
-    
     addEventListener('keydown', (e) => {
         const allFocusEls = document.querySelectorAll('[id]');
         let letter = e.key.toLowerCase();
 
+        
         if(!questionAnswersFocused){
             if (letter == 'm' && e.target.id == 'mainTargetDiv'){
                 scrollTo(0,0)
@@ -44,10 +44,61 @@ export function letterFocus() {
             // Update current letter for next key press
             currentLetter = letter;
         } else {
+            if(!isNaN(letter)){
+                questionNumFocus(e,letter)
+            }
             return
         }
-
+        
     });
+    questionAnswers.forEach(el => {
+        el.addEventListener('focusin', e => {
+            questionAnswersFocused = true
+            console.log('in')
+        })
+        el.addEventListener('focus', e => {
+            questionAnswersFocused = true
+            console.log('focus')
+        })
+        el.addEventListener('focusout', e => {
+            questionAnswersFocused = false
+        })
+    })
+}
+
+function questionNumFocus(e,letter){
+    // get question-answer parent of current element when .question-txt .dropQuestion has focus and answers
+    // are showing
+    const intLet = parseInt(letter)
+    const questionAnswer = getQuestionAnswer(e.target.parentElement)
+    const answerTxt = questionAnswer.querySelector('.answer-txt')
+    let hiddenAnswerTxt = true ? answerTxt.classList.contains('hide') : false
+    if(!hiddenAnswerTxt){
+        const h3s = answerTxt.querySelectorAll('h3')
+        if(intLet > h3s.length){
+            return
+        }
+        h3s.forEach(h3 => {
+            h3.setAttribute('tabindex',0)
+        })
+        h3s[intLet - 1].focus()
+
+    }
+}
+
+function removeTabIndexes(els) {
+    els.forEach(el => {
+        el.removeAttribute('tabindex')
+    })
+}
+function getQuestionAnswer(parent){
+    if(parent.classList.contains('question-answer')){
+        return parent
+    } else if (parent.parentElement){
+        return getQuestionAnswer(parent.parentElement)
+    } else {
+        return null
+    }
 }
 
 letterFocus();
