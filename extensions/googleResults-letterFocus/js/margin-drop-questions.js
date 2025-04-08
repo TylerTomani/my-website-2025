cf31db619639fd4930aa93540046401b2809108d
Tyler Tomani <ttomani@gmail.com>
2025-04-08 03:33:54 -0400

    main

--- extensions/googleResults-letterFocus/js/margin-drop-questions.js
+++ extensions/googleResults-letterFocus/js/margin-drop-questions.js
@@ -2,17 +2,11 @@
 const dropQuestions = document.querySelectorAll('.drop-question')
 dropQuestions.forEach((el,i) => {
     if(i == 0 || i % 2 == 0){
-        el.style.marginRight = '10%'
+        el.style.marginLeft = '30%';
     }  else {
-        el.parentElement.style.position = 'relative'
-        console.log(el.parentElement)
-        el.parentElement.style.alignItems = 'flex-start'
-        el.parentElement.style.justifyContent = 'flex-start'
-        el.parentElement.style.border = '5px solid lime'
-        el.style.marginRight = '150%';
-        // el.style.marginLeft = '10%'
-        // el.style.marginLeft = '10%'
-        
+        el.style.marginRight = '30%';
+        el.style.marginLeft = '3%';
+
     }
 })
 })();
